176359fc2b0f72640f60b043fcb2a0ba96050104
veedev25 <76547916+veedev25@users.noreply.github.com>
2022-09-23 21:59:58 +0530

    Update nuclei.sh

--- modules/nuclei.sh
+++ modules/nuclei.sh
@@ -6,7 +6,7 @@ url=$1
 echo "Nuclei Scan" >> /app/results/$url-output.txt
 printf "\n\n" >> /app/results/$url-output.txt
 
-/app/modules/binaries/nuclei -t /app/modules/nuclei-template/ -u $url -severity critical,high,medium,low | tee -a /app/results/$url-output.txt
+/app/modules/binaries/nuclei -u $url -t /app/modules/nuclei-template/ -severity critical,high,medium,low | tee -a /app/results/$url-output.txt
 
 printf "\n\n\n" >> /app/results/$url-output.txt
 printf "##########################################################################################\n" >> /app/results/$url-output.txt
